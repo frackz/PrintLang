247e8e8abe62f767cc4d799d34634ee8121939ea
MBGameDev123 <98520425+MBGameDev123@users.noreply.github.com>
2022-08-08 09:57:21 +0200

    Update printlang.js

--- printlang.js
+++ printlang.js
@@ -5,8 +5,7 @@ const Hashmap = require('hashmap')
 
 const Variables = new Hashmap()
 
-
-const file = process.argv[2]
+const file = "test.pg"
 if (file == null) {return console.error("Cannot find the specified file")}
 if (!file.endsWith('.pg')) {return console.error("File needs to end with .pg")}
 try {if (!require('fs').existsSync(file)) {return console.error("The specified file does not exist.")}} catch(err) {console.error(err)}
@@ -24,8 +23,14 @@ splitted.forEach(function(contents) {
     CurrentLine++;
     if (contents.startsWith('###')) {}
     else if (contents == '') {}
+    else if (contents.startsWith('edit')) {
+      VariableManager.edit(contents)
+    }
+    else if (contents.startsWith('print')) {console.log(VariableManager.load(contents))}
+    else if (contents.startsWith('delete')) {
+      VariableManager.delete(contents)
+    }
     else if (contents.startsWith('var')) {VariableManager.init(contents, CurrentLine)} 
     else if(contents.startsWith('loop')) {LoopManager.loop(contents, CurrentLine)} 
-    else {console.log(VariableManager.load(contents))}
+    else {return console.error("Syntax not found, please double check the docs")}
 })
-
