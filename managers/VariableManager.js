3523a094acb6b688b9e015c11d0e75b89fcd3cb6
MBGameDev123 <98520425+MBGameDev123@users.noreply.github.com>
2022-08-08 09:58:12 +0200

    Update VariableManager.js

--- managers/VariableManager.js
+++ managers/VariableManager.js
@@ -5,6 +5,48 @@ module.exports = class VariableManager {
         this.vars = vars
     }
 
+    edit(line) {
+      const values = line.split(" ")
+      if (!values[0] == 'edit') {
+        return console.error("Syntax error, delete")
+      }
+      if (this.vars.get(values[1]) == false) {
+        return console.error("Edit variable is invalid")
+      }
+
+      if (values[2] != '=') {
+        return console.error("Edited variables needs = for value")
+      }
+      if (values.length < 3) {
+        return console.error("Needs variable new value")
+      } 
+      var contents = values[3]
+      if (values.length > 4) {
+          contents = ''
+          for (var i = 0; i < values.length-3; i++) {
+              if (values.length == i+4) {contents+=values[i+3]} else {contents+=values[i+3]+' '}
+          }
+      }
+      contents = contents.replace(/(\r\n|\n|\r)/gm, "");
+      this.vars.set(values[1], contents)
+    }
+
+    delete(line) {
+      const values = line.split(" ")
+      if (!values[0] == 'delete') {
+        return console.error("Syntax error, delete")
+      }
+      if (values[1] == '') {
+        return console.error("Delete variable is invalid")
+      }
+
+      if (this.vars.get(values[1]) == false) {
+        return console.error("Deleted variable doesnt exist")
+      }
+
+      this.vars.remove(values[1])
+    }
+  
     init(line, num) {
         const values = line.split(' ')
 
@@ -27,6 +69,9 @@ module.exports = class VariableManager {
     }
 
     load(line) {
+        if (!line.startsWith("print")) {return console.error("Syntax not loaded")}
+        const splitted = line.split(" ")
+        if (!splitted[1] == "=") {return console.error("Neet a = after printing...")}
         var returnLine = line
         for (var i = 0; i < checkOccurences(line, "#var="); i++) {
             returnLine=returnLine.replace(/(\r\n|\n|\r)/gm, "");
@@ -34,6 +79,6 @@ module.exports = class VariableManager {
             if (!this.vars.has(name)) {return console.error(`Variable doesn't exist, with name ${name}`)}
             returnLine = returnLine.replace(`#var=${name}`, this.vars.get(name))
         }
-        return returnLine
+        return returnLine.replace("print = ","")
     }
 }
